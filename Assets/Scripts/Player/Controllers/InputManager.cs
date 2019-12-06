29735878a1d95f3842d5c61872ffe60d743d05f1
John Gotts <44810602+Mitoridori@users.noreply.github.com>
2019-12-06 15:57:19 -0500

    started work on the input manager for the player
    started working on the input manager which will act as an intermediary between the controllers and the input section of the engine
    
    added the inputs to Unity input manager for the keys that we need for the project

---
+++ Assets/Scripts/Player/Controllers/InputManager.cs
@@ -0,0 +1,52 @@
+﻿//FILE: InputManager.cs
+//PROJECT: Will Of The Woods
+//PROGRAMMER: John Gotts
+//FIRST VERSION: 06/12/2019
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NAME: InputManager
+//PURPOSE: This class is meant to act as an intermediary
+// between the default input manager and our controllers.
+public class InputManager : MonoBehaviour
+{
+
+    public delegate void InputEvent();
+
+    public static event InputEvent Jump;
+    public static event InputEvent Walk;
+    public static event InputEvent Interact;
+    public static event InputEvent Rotate;
+
+    //Function: TriggerJump
+    //DESCRIPTION: Calls the event for jumping
+    //PARAMETERS: None
+    //RETURNS: None
+    public static void TriggerJump()
+    {
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+    }
+
+    //Function: TriggerWalk
+    //DESCRIPTION: Calls the event for walking forward / backward
+    //PARAMETERS: None
+    //RETURNS: None
+    public static void TriggerWalk()
+    {
+        if (Input.GetAxis("Vertical") != 0)
+            Walk();
+    }
+
+    //Function: TriggerInteract
+    //DESCRIPTION: Calls the event for interacting
+    //PARAMETERS: None
+    //RETURNS: None
+    public static void TriggerInteract()
+    {
+        if (Input.GetButtonDown("Interact"))
+            Interact();
+    }
+
+}
