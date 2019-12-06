6b6fe2b6a2b4cc303af6ad0ac948534dbbcd271a
John Gotts <44810602+Mitoridori@users.noreply.github.com>
2019-12-06 16:10:01 -0500

    Update InputManager.cs
    more work on the input manager, making more events to be coded in still

--- Assets/Scripts/Player/Controllers/InputManager.cs
+++ Assets/Scripts/Player/Controllers/InputManager.cs
@@ -18,6 +18,18 @@ public class InputManager : MonoBehaviour
     public static event InputEvent Walk;
     public static event InputEvent Interact;
     public static event InputEvent Rotate;
+    public static event InputEvent Attack;
+
+    public static event InputEvent Ability1;
+    public static event InputEvent Ability2;
+    public static event InputEvent Ability3;
+    public static event InputEvent Ability4;
+    public static event InputEvent Ability5;
+    public static event InputEvent Ability6;
+    public static event InputEvent Ability7;
+    public static event InputEvent Ability8;
+    public static event InputEvent Ability9;
+    public static event InputEvent Ability0;
 
     //Function: TriggerJump
     //DESCRIPTION: Calls the event for jumping
@@ -47,6 +59,17 @@ public class InputManager : MonoBehaviour
     {
         if (Input.GetButtonDown("Interact"))
             Interact();
+    }
+
+    //Function: TriggerRotate
+    //DESCRIPTION: Calls the event for rotating left or right
+    //PARAMETERS: None
+    //RETURNS: None
+    public static void TriggerRotate()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+            Rotate();
+
     }
 
 }
