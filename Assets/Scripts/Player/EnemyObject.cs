875db7298201d6e9cd85acb088512c3df2119a13
eitrunix <52928831+eitrunix@users.noreply.github.com>
2019-12-06 16:22:32 -0500

    EntityClass Initial Setup
    Setup Initial EnitityObject Class, PlayerObject and EnemyObject scripts and Objects
    Will need to Add in Stats after this to give the player some substance other than data in the program

---
+++ Assets/Scripts/Player/EnemyObject.cs
@@ -0,0 +1,141 @@
+﻿
+//File Header:
+//FILE : EnenmyObject.cs
+////PROJECT : WillOfTheWood
+//PROGRAMMER : Jeff
+//FIRST VERSION : 06/12/2019
+
+using UnityEngine;
+[CreateAssetMenu(menuName = "Entity/Enemy", fileName = "Entities")]
+public class EnemyObject : EntityObject
+{
+    // Uncomment when we have the Stats Setup in game
+
+    //public Stats health;
+    //public Stats accuracy;
+    //public Stats cunning;
+    //public Stats discrete;
+    //public Stats persuasive;
+    //public Stats quick;
+    //public Stats strong;
+    //public Stats vigilant;
+
+    public int eHealth;
+    public int eAccuracy;
+    public int eCunning;
+    public int eDiscrete;
+    public int ePersuasive;
+    public int eQuick;
+    public int eStrong;
+    public int eVigilant;
+
+////FUNCTION : Awake()
+////DESCRIPTION : Sets the EnemyObjects Stats to the Default BaseStats
+////PARAMETERS : VOID
+////RETURNS : VOID 
+
+    private void Awake()
+    {
+        //eHealth = health.statValue;
+        //eAccuracy = accuracy.statValue;
+        //eCunning = cunning.statValue;
+        //eDiscrete = discrete.statValue;
+        //ePersuasive = persuasive.statValue;
+        //eStrong = strong.statValue;
+        //eVigilant = vigilant.statValue;
+
+        GetStats();
+
+    }
+    //FUNCTION : GetHealth/GetAccuracy/GetCunning/GetDiscrete/GetPersuasive/GetQuick/GetStrong/GetVigilant
+    //DESCRIPTION :Gets the enemies Called Stat
+    //PARAMETERS : VOID
+    //RETURNS : eHealth, eAccuracy, eCunning, eDiscrete, ePersuasive, eQuick, eStrong, eVigilant
+    public int GetHealth()
+    {
+        return eHealth;
+    }
+
+    //FUNCTION : SetHealth/SetAccuracy/SetCunning/SetDiscrete/SetPersuasive/SetQuick/SetStrong/SetVigilant
+    //DESCRIPTION : Setting the Stat to the Value we put in.
+    //PARAMETERS : int _value
+    //RETURNS : 
+
+    public void SetHealth(int _value)
+    {
+        _value = eHealth;
+    }
+    public int GetAccuracy()
+    {
+        return eAccuracy;
+    }
+    public void SetAccuracy(int _value)
+    {
+        _value = eAccuracy;
+    }
+    public int GetCunning()
+    {
+        return eCunning;
+    }
+    public void SetCunning(int _value)
+    {
+        _value = eCunning;
+    }
+    public int GetDiscrete()
+    {
+        return eDiscrete;
+    }
+    public void SetDiscrete(int _value)
+    {
+        _value = eDiscrete;
+    }
+    public int GetQuick()
+    {
+        return eQuick;
+    }
+    public void SetQuick(int _value)
+    {
+        _value = eQuick;
+    }
+    public int GetPersuasive()
+    {
+        return ePersuasive;
+    }
+    public void SetPersuasive(int _value)
+    {
+        _value = ePersuasive;
+    }
+    public int GetStrong()
+    {
+        return eStrong;
+    }
+    public void SetStrong(int _value)
+    {
+        _value = eStrong;
+    }
+    public int GetVigilant()
+    {
+        return eVigilant;
+    }
+    public void SetVigilant(int _value)
+    {
+        _value = eVigilant;
+    }
+
+    //FUNCTION : GetStats
+    //DESCRIPTION : Function to get all of the enemies stats
+    //PARAMETERS : VOID
+    //RETURNS : VOID
+    public void GetStats()
+    {
+        GetHealth();
+        GetAccuracy();
+        GetCunning();
+        GetDiscrete();
+        GetPersuasive();
+        GetQuick();
+        GetStrong();
+        GetVigilant();
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerObject.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu (menuName = "Quests/KillQuest", fileName = "KillQuests")]
+public class PlayerObject : EntityObject
+{
+    //public Stats health;
+    //public Stats accuracy;
+    //public Stats cunning;
+    //public Stats discrete;
+    //public Stats persuasive;
+    //public Stats quick;
+    //public Stats strong;
+    //public Stats vigilant;
+
+    public int pHealth;
+    public int pAccuracy;
+    public int pCunning;
+    public int pDiscrete;
+    public int pPersuasive;
+    public int pQuick;
+    public int pStrong;
+    public int pVigilant;
+
+    //public PlayerController player;
+    //Other Player Needed things.
+
+    private void Awake()
+    {
+        //pHealth = health.statValue;
+        //pAccuracy = accuracy.statValue;
+        //pCunning = cunning.statValue;
+        //pDiscrete = discrete.statValue;
+        //pPersuasive = persuasive.statValue;
+        //pStrong = strong.statValue;
+        //pVigilant = vigilant.statValue;
+
+        GetStats();
+    }
+    
+    public int GetHealth()
+    {
+        return pHealth;
+    }
+    public void SetHealth(int _value)
+    {
+        _value = pHealth;
+    }
+    public int GetAccuracy()
+    {
+        return pAccuracy;
+    }
+    public void SetAccuracy(int _value)
+    {
+        _value = pAccuracy;
+    }
+    public int GetCunning()
+    {
+        return pCunning;
+    }
+    public void SetCunning(int _value)
+    {
+        _value = pCunning;
+    }
+    public int GetDiscrete()
+    {
+        return pDiscrete;
+    }
+    public void SetDiscrete(int _value)
+    {
+        _value = pDiscrete;
+    }
+    public int GetQuick()
+    {
+        return pQuick;
+    }
+    public void SetQuick(int _value)
+    {
+        _value = pQuick;
+    }
+    public int GetPersuasive()
+    {
+        return pPersuasive;
+    }
+    public void SetPersuasive(int _value)
+    {
+        _value = pPersuasive;
+    }
+    public int GetStrong()
+    {
+        return pStrong;
+    }
+    public void SetStrong(int _value)
+    {
+        _value = pStrong;
+    }
+    public int GetVigilant()
+    {
+        return pVigilant;
+    }
+    public void SetVigilant(int _value)
+    {
+        _value = pVigilant;
+    }
+    public void GetStats()
+    {
+        GetHealth();
+        GetAccuracy();
+        GetCunning();
+        GetDiscrete();
+        GetPersuasive();
+        GetQuick();
+        GetStrong();
+        GetVigilant();
+    }
+
+}
