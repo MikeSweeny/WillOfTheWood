26d78afe477264a1403ad575125702403e9109c7
Gavin McGuire <44212243+Milowan@users.noreply.github.com>
2019-12-06 15:43:36 -0500

    added BattleUnit class
    base class to be used to back-end logic of the final battle

---
+++ Assets/Scripts/World/BattleUnit.cs
@@ -0,0 +1,40 @@
+﻿//FILE          :   BattleUnit.cs
+//PROJECT       :   Will of the Woods
+//PROGRAMMER    :   Gavin McGuire
+//FIRST VERSION :   06/12/2019
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NAME : BattleUnit
+//PURPOSE : interact with other units for the final battle
+public class BattleUnit
+{
+    private int unitTier;
+    private const int MINRANGE = 0;
+    private const int MAXRANGE = 20;
+
+
+    public BattleUnit(int tier)
+    {
+        unitTier = tier;
+    }
+
+    //Function : SkillCheck
+    //DESCRIPTION : adds a random value in range to unit tier
+    //PARAMETERS : none
+    //RETURNS : int : indicating battle performance
+    public int SkillCheck()
+    {
+        return (unitTier + Random.Range(MINRANGE, MAXRANGE));
+    }
+
+    //Function : Weaken
+    //DESCRIPTION : lowers unit's tier by a set amount
+    //PARAMETERS : int amount : the amount by which to reduce the tier
+    //RETURNS : none
+    public void Weaken(int amount)
+    {
+        unitTier -= amount;
+    }
+}
