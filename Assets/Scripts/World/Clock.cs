7a7387a8d5fb2277f53cb9d78b1dc1480f98a57e
TimothyOlenio <52928854+TimothyOlenio@users.noreply.github.com>
2019-12-06 15:13:28 -0500

    Created Clock and Instance Manager Script
    The Clock increments the seconds and increment the day when the seconds build to a point. This clock also calls the day and night trigger
    
    The Instance manager simply calls the Clock method

---
+++ Assets/Scripts/World/Clock.cs
@@ -0,0 +1,39 @@
+﻿//FILE : Clock.cs
+//PROJECT : Will of the Woods
+//PROGRAMMER : Timothy Olenio
+//FIRST VERSION : 06/12/2019
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NAME : Clock
+//PURPOSE : This class manages the clock and progresses the day
+public class Clock : MonoBehaviour
+{
+    static int currentDay = 1;
+    static int hours;
+    static float seconds;
+
+    public static void UpdateClock(float deltaTime)
+    {
+        seconds += 24 * deltaTime;
+        Debug.Log(seconds);
+
+        if (seconds >= 3600)
+        {
+            hours++;
+            seconds = 0;
+        }
+
+        if (hours == 16)
+        {
+            GameEventManager.TriggerStartNight();
+        }
+        if (hours >= 24)
+        {
+            GameEventManager.TriggerStartDay();
+            hours = 0;
+            currentDay++;
+        }
+    }
+}
---
+++ Assets/Scripts/World/InstanceManager.cs
@@ -0,0 +1,24 @@
+﻿//FILE : InstanceManager.cs
+//PROJECT : Will of the Woods
+//PROGRAMMER : Timothy Olenio
+//FIRST VERSION : 06/12/2019
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NAME : InstanceManager
+//PURPOSE : This class calls the clock to update
+public class InstanceManager : MonoBehaviour
+{
+    float deltaTime;
+    private void Start()
+    {
+        deltaTime = Time.deltaTime;
+
+    }
+
+    private void Update()
+    {
+        Clock.UpdateClock(deltaTime);
+    }
+}
