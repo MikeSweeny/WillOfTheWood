0cd55ea5a3c9ef935187e5f62a8729ad15c12115
John Gotts <44810602+Mitoridori@users.noreply.github.com>
2019-12-09 14:26:35 -0500

    refactoring and renaming
    refactored the input manager to better fit team standards
    
    renamed inputs in unity input manager to reflect the refactored names

--- Assets/Scripts/Player/Controllers/InputManager.cs
+++ Assets/Scripts/Player/Controllers/InputManager.cs
@@ -24,20 +24,23 @@ public class InputManager : MonoBehaviour
     public static event InputEvent Jump;
     public static event InputEvent Interact;
     public static event InputEvent Attack;
-    public static event InputEvent Ability1;
-    public static event InputEvent Ability2;
-    public static event InputEvent Ability3;
-    public static event InputEvent Ability4;
-    public static event InputEvent Ability5;
-    public static event InputEvent Ability6;
-    public static event InputEvent Ability7;
-    public static event InputEvent Ability8;
-    public static event InputEvent Ability9;
-    public static event InputEvent Ability0;
+    public static event InputEvent HotbarSlot1;
+    public static event InputEvent HotbarSlot2;
+    public static event InputEvent HotbarSlot3;
+    public static event InputEvent HotbarSlot4;
+    public static event InputEvent HotbarSlot5;
+    public static event InputEvent HotbarSlot6;
+    public static event InputEvent HotbarSlot7;
+    public static event InputEvent HotbarSlot8;
+    public static event InputEvent HotbarSlot9;
+    public static event InputEvent HotbarSlot0;
 
     public static event WalkInputEvent Walk;
     public static event RotateInputEvent Rotate;
     public static event CamerControlEvent MoveCamera;
+
+    //set to false when menu is opened
+    public static bool inputEnabled = true;
 
     private void Awake()
     {
@@ -54,9 +57,46 @@ public class InputManager : MonoBehaviour
 
     private void Update()
     {
-        TriggerJump();
         MoveCamera?.Invoke();
-        TriggerAttack();
+
+        if (Input.GetButtonDown("Jump"))
+            TriggerJump();
+
+        if (Input.GetMouseButtonDown(0))
+            TriggerAttack();
+
+        if (Input.GetButtonDown("Interact"))
+            TriggerInteract();
+
+        if (Input.GetButtonDown("HotbarSlot0"))
+            UseHotBarSlot0();
+
+        if (Input.GetButtonDown("HotbarSlot1"))
+            UseHotBarSlot1();
+
+        if (Input.GetButtonDown("HotbarSlot2"))
+            UseHotBarSlot2();
+
+        if (Input.GetButtonDown("HotbarSlot3"))
+            UseHotBarSlot3();
+
+        if (Input.GetButtonDown("HotbarSlot4"))
+            UseHotBarSlot4();
+
+        if (Input.GetButtonDown("HotbarSlot5"))
+            UseHotBarSlot5();
+
+        if (Input.GetButtonDown("HotbarSlot6"))
+            UseHotBarSlot6();
+
+        if (Input.GetButtonDown("HotbarSlot7"))
+            UseHotBarSlot7();
+
+        if (Input.GetButtonDown("HotbarSlot8"))
+            UseHotBarSlot8();
+
+        if (Input.GetButtonDown("HotbarSlot9"))
+            UseHotBarSlot9();
     }
 
     private void FixedUpdate()
@@ -72,7 +112,7 @@ public class InputManager : MonoBehaviour
     //RETURNS: None
     public static void TriggerJump()
     {
-        if (Input.GetButtonDown("Jump"))
+        if(inputEnabled)
             Jump?.Invoke();
     }
 
@@ -82,7 +122,7 @@ public class InputManager : MonoBehaviour
     //RETURNS: None
     public static void TriggerInteract()
     {
-        if (Input.GetButtonDown("Interact"))
+        if (inputEnabled)
             Interact?.Invoke();
     }
 
@@ -92,108 +132,107 @@ public class InputManager : MonoBehaviour
     //RETURNS: None
     public static void TriggerAttack()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (inputEnabled)
             Attack?.Invoke();
-
     }
 
     //Function: TriggerAbility1
     //DESCRIPTION: Calls the event for the ability in the 1 slot
     //PARAMETERS: None
     //RETURNS: None
-    public static void TriggerAbility1()
+    public static void UseHotBarSlot1()
     {
-        if(Input.GetButtonDown("Ability1"))
-            Ability1?.Invoke();
+        if (inputEnabled)
+            HotbarSlot1?.Invoke();
     }
 
     //Function: TriggerAbility2
     //DESCRIPTION: Calls the event for the ability in the 2 slot
     //PARAMETERS: None
     //RETURNS: None
-    public static void TriggerAbility2()
+    public static void UseHotBarSlot2()
     {
-        if (Input.GetButtonDown("Ability2"))
-            Ability2?.Invoke();
+        if (inputEnabled)
+            HotbarSlot2?.Invoke();
     }
 
     //Function: TriggerAbility3
     //DESCRIPTION: Calls the event for the ability in the 3 slot
     //PARAMETERS: None
     //RETURNS: None
-    public static void TriggerAbility3()
+    public static void UseHotBarSlot3()
     {
-        if (Input.GetButtonDown("Ability3"))
-            Ability3?.Invoke();
+        if (inputEnabled)
+            HotbarSlot3?.Invoke();
     }
 
     //Function: TriggerAbility4
     //DESCRIPTION: Calls the event for the ability in the 4 slot
     //PARAMETERS: None
     //RETURNS: None
-    public static void TriggerAbility4()
+    public static void UseHotBarSlot4()
     {
-        if (Input.GetButtonDown("Ability4"))
-            Ability4?.Invoke();
+        if (inputEnabled)
+            HotbarSlot4?.Invoke();
     }
 
     //Function: TriggerAbility5
     //DESCRIPTION: Calls the event for the ability in the 5 slot
     //PARAMETERS: None
     //RETURNS: None
-    public static void TriggerAbility5()
+    public static void UseHotBarSlot5()
     {
-        if (Input.GetButtonDown("Ability5"))
-            Ability5?.Invoke();
+        if (inputEnabled)
+            HotbarSlot5?.Invoke();
     }
 
     //Function: TriggerAbility6
     //DESCRIPTION: Calls the event for the ability in the 6 slot
     //PARAMETERS: None
     //RETURNS: None
-    public static void TriggerAbility6()
+    public static void UseHotBarSlot6()
     {
-        if (Input.GetButtonDown("Ability6"))
-            Ability6?.Invoke();
+        if (inputEnabled)
+            HotbarSlot6?.Invoke();
     }
 
     //Function: TriggerAbility7
     //DESCRIPTION: Calls the event for the ability in the 7 slot
     //PARAMETERS: None
     //RETURNS: None
-    public static void TriggerAbility7()
+    public static void UseHotBarSlot7()
     {
-        if (Input.GetButtonDown("Ability7"))
-            Ability7?.Invoke();
+        if (inputEnabled)
+            HotbarSlot7?.Invoke();
     }
 
     //Function: TriggerAbility8
     //DESCRIPTION: Calls the event for the ability in the 8 slot
     //PARAMETERS: None
     //RETURNS: None
-    public static void TriggerAbility8()
+    public static void UseHotBarSlot8()
     {
-        if (Input.GetButtonDown("Ability8"))
-            Ability8?.Invoke();
+        if (inputEnabled)
+            HotbarSlot8?.Invoke();
     }
 
     //Function: TriggerAbility9
     //DESCRIPTION: Calls the event for the ability in the 9 slot
     //PARAMETERS: None
     //RETURNS: None
-    public static void TriggerAbility9()
+    public static void UseHotBarSlot9()
     {
-        if (Input.GetButtonDown("Ability9"))
-            Ability9?.Invoke();
+        if (inputEnabled)
+            HotbarSlot9?.Invoke();
     }
 
     //Function: TriggerAbility0
     //DESCRIPTION: Calls the event for the ability in the 0 slot
     //PARAMETERS: None
     //RETURNS: None
-    public static void TriggerAbility0()
+    public static void UseHotBarSlot0()
     {
-        if (Input.GetButtonDown("Ability0"))
-            Ability0?.Invoke();
+        if (inputEnabled)
+            HotbarSlot0?.Invoke();
     }
 }
