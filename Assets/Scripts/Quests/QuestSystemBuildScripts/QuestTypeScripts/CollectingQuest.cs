83fff9bfc3816d60f329beee2250bba26c3e8000
Rebecca Stewart <44781452+beckrpgirl@users.noreply.github.com>
2019-12-09 20:14:21 -0500

    Questing system
    Adding in the start to the Questing system, and Quest Manager

---
+++ Assets/Scripts/Quests/QuestSystemBuildScripts/QuestTypeScripts/CollectingQuest.cs
@@ -0,0 +1,42 @@
+﻿//FILE : CollectingQuest.cs
+//PROJECT : Will of the Woods
+//PROGRAMMER : Rebecca Stewart
+//FIRST VERSION : 09/12/2019
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NAME : CollectingQuest
+//PURPOSE : Giving the quests function the correct information for the current quest
+public class CollectingQuest : QuestGoal
+{
+    public string ItemID { get; set; }
+
+    public CollectingQuest(Quests quest, string itemID, string description, bool completed, int currentAmount, int requiredAmount, int coins)
+    {
+        this.Quest = quest;
+        this.ItemID = itemID;
+        this.Description = description;
+        this.Completed = completed;
+        this.CurrentAmount = currentAmount;
+        this.RequiredAmount = requiredAmount;
+        this.CoinReward = coins;
+
+    }
+
+    public override void Init()
+    {
+        base.Init();
+        QuestEvents.EndEvent += ItemCollected;
+    }
+
+    void ItemCollected(IQuestID items)
+    {
+        if (items.ID == this.ItemID)
+        {
+            this.CurrentAmount++;
+            Evaluate();
+        }
+    }
+}
---
+++ Assets/Scripts/Quests/QuestSystemBuildScripts/QuestTypeScripts/KillQuest.cs
@@ -0,0 +1,41 @@
+﻿//FILE : KillQuest.cs
+//PROJECT : Will of the Woods
+//PROGRAMMER : Rebecca Stewart
+//FIRST VERSION : 09/12/2019
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NAME : KillQuest
+//PURPOSE : Giving the quests function the correct information for the current quest
+public class KillQuest : QuestGoal
+{
+    public string EnemyID { get; set; }
+
+    public KillQuest(Quests quest, string enemyID, string description, bool completed, int currentAmount, int requiredAmount, int coins)
+    {
+        this.Quest = quest;
+        this.EnemyID = enemyID;
+        this.Description = description;
+        this.Completed = completed;
+        this.CurrentAmount = currentAmount;
+        this.RequiredAmount = requiredAmount;
+        this.CoinReward = coins;
+
+    }
+
+    public override void Init()
+    {
+        base.Init();
+        QuestEvents.EndEvent += EnemyDied;
+    }
+
+    void EnemyDied(IQuestID enemy)
+    {
+        if (enemy.ID == this.EnemyID)
+        {
+            this.CurrentAmount++;
+            Evaluate();
+        }
+    }
+}
