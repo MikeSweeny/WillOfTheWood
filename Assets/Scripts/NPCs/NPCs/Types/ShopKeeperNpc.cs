19b91036108bf3221f7361f453bf7a73d54d54d5
John Gotts <44810602+Mitoridori@users.noreply.github.com>
2019-12-12 14:22:17 -0500

    made the test quest giver NPC and made changes to the player for interacting
    made the base script for the interactable npcs and 2 children, the quest giver and the shop keeper
    
    removed the readme as actual files were added to the folder it was in
    
    added comments to code that needed them
    
    made the interact for the player use raycasts, this raycast looks for an object in front of the player and then checks to see if its tag matches certain tags, will then call that objects interact function

---
+++ Assets/Scripts/NPCs/NPCs/Types/ShopKeeperNpc.cs
@@ -0,0 +1,23 @@
+﻿//FILE: ShopKeeperNpc.cs
+//PROJECT: Will Of The Woods
+//PROGRAMMER: John Gotts
+//FIRST VERSION: 12/12/2019
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NAME: ShopKeeperNpc
+//PURPOSE: this class will outline what is supposed to happen with the shop NPCs
+public class ShopKeeperNpc : BaseInteractableNpc
+{
+    //Function: OnInteract
+    //DESCRIPTION: this function is an override from the parent class so that this npc knows what to do when its interacted with
+    //PARAMETERS: None
+    //RETURNS: None
+    public override void OnInteract()
+    {
+        
+    }
+
+}
