6034da259cd9018b2408fcd116e9f2f6f982e7ab
Gavin McGuire <44212243+Milowan@users.noreply.github.com>
2019-12-12 14:08:03 -0500

    Made Buttons tor represent main menu functionality
    buttons needed a script attached to them in order to be able to call functions from the UIEventManager

---
+++ Assets/Scripts/UI/Buttons/UOptionsButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UOptionsButton : UIButton
+{
+    public override void Clicked()
+    {
+        throw new System.NotImplementedException();
+    }
+}
