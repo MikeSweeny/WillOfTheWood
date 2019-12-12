54a33a5253a1e942dc6c4744ff1da20450c4f481
Gavin McGuire <44212243+Milowan@users.noreply.github.com>
2019-12-12 16:30:49 -0500

    added comments to save related classes

--- Assets/Scripts/World/GameSaveManager.cs
+++ Assets/Scripts/World/GameSaveManager.cs
@@ -1,7 +1,13 @@
-﻿using System.Collections;
+﻿//FILE          :   GameSaveManager.cs
+//PROJECT       :   Will of the Wood
+//PROGRAMMER    :   Gavin McGuire
+//FIRST VERSION :   12/12/2019
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//NAME : GameSaveManager
+//PURPOSE : manages the save slots for the game
 public class GameSaveManager
 {
 
--- Assets/Scripts/World/SaveSlot.cs
+++ Assets/Scripts/World/SaveSlot.cs
@@ -1,7 +1,13 @@
-﻿using System.Collections;
+﻿//FILE          :   SaveSlot.cs
+//PROJECT       :   Will of the Wood
+//PROGRAMMER    :   Gavin McGuire
+//FIRST VERSION :   DD/MM/YYYY
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//NAME : SaveSlot
+//PURPOSE : holds the save information to a game session
 public class SaveSlot
 {
     private bool empty;
