ae95b8ac942274436b06e0b9e5e9c15d8e760649
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2019-12-12 15:10:20 -0500

    Reimported Quest Scripts
    -Deleted the old existing files for the quest scripts.
    -Setup the folders and added the updated scripts.
    -Commented out the UI portions of the code that were giving errors
    -Added the Scriptable Objects for quests that were already created.
    * Ready for the UI Team to have a look and let us know what they need. *

--- Assets/Scripts/NPCs/NPCs/Types/QuestGiverNpc.cs
+++ Assets/Scripts/NPCs/NPCs/Types/QuestGiverNpc.cs
@@ -11,13 +11,121 @@ using UnityEngine;
 //PURPOSE: this class is used to tell the quest givers what they are supposed to do when they are interacted with
 public class QuestGiverNpc : BaseInteractableNpc
 {
+    //UI and Quests.cs to link to
+    //QuestUIManager QUIM;
+    QuestManager QM;
+    public Quests Quest { get; set; }
+    public bool AssignedQuest { get; set; } //Has quest been assigned
+    public bool Helped { get; set; } //quest to hand in
+    public List<Quests> QuestList = new List<Quests>(); //List of quests for NPC
+    private int i = 0; //quest counter
+
+
+    void Awake()
+    {
+        //QUIM = FindObjectOfType<QuestUIManager>();
+        QM = FindObjectOfType<QuestManager>();
+    }
     //Function: OnInteract
     //DESCRIPTION: this function is used to say what the quest giver does when it is interacted with
     //PARAMETERS: None
     //RETURNS: None
     public override void OnInteract()
     {
-        
+
+        if (!AssignedQuest && !Helped)
+        {
+            //if (QUIM)
+            //{
+            //    QUIM.NPCBoxOne.text = "Welcome to Wonderland!";
+            //    QUIM.NPCBoxTwo.text = "Will you please help me out with my quest?";
+            //}
+
+            AssignQuest();
+        }
+        else if (AssignedQuest && !Helped)
+        {
+            CheckQuest();
+        }
+        else
+        {
+            NextQuest();
+        }
+
     }
 
+    //FUNCTION : AssignedQuest
+    //DESCRIPTION : Assigns the quest to the player if needed
+    void AssignQuest()
+    {
+
+
+        //checking to see if valid 
+
+        for (int i = 0; i < QuestList.Count; i++)
+        {
+            if (!QM.searchCQNList(QuestList[i].QuestName))
+            {
+                if (QM.searchCQNList(QuestList[i].Prereq1) && QM.searchCQNList(QuestList[i].Prereq2))
+                {
+                    Quest = QuestList[i];
+
+                    AssignedQuest = true;
+                    Quest.Load();
+                    Quest.StartText();
+                    return;
+                }
+            }
+        }
+        //NoMoreQuest();
+    }
+
+    //FUNCTION : CheckQuest
+    //DESCRIPTION : Checking to see if quest is done or not and assigning the correct dialoge/action
+    void CheckQuest()
+    {
+        if (Quest.Completed)
+        {
+            Quest.GiveReward();
+            Helped = true;
+            AssignedQuest = false;
+            QM.addToCQNList(Quest.QuestName);
+            Quest.CompletedText();
+
+        }
+        else
+        {
+            Quest.InprogressText();
+        }
+    }
+
+    //FUNCTION : NextQuest
+    //DESCRIPTION : Calls the next quest in the line when the player revisits the questGiver
+    void NextQuest()
+    {
+        i++;
+        if (i >= QuestList.Count)
+        {
+            //NoMoreQuest();
+        }
+        else
+        {
+            Helped = false;
+            AssignedQuest = false;
+            AssignQuest();
+
+        }
+    }
+
+    //FUNCTION : NoMoreQuest
+    //DESCRIPTION : Calls the dialog if no more quests are to be found
+    //void NoMoreQuest()
+    //{
+    //    if (QUIM)
+    //    {
+    //        QUIM.NPCBoxOne.text = "Welcome to Wonderland!";
+    //        QUIM.NPCBoxTwo.text = "No Available Quests";
+    //    }
+    //    Debug.Log("No Quest Got");
+    //}
 }
