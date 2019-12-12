218808dfba37a9487e527355d9b0818c38a8436b
Gavin McGuire <44212243+Milowan@users.noreply.github.com>
2019-12-12 14:32:05 -0500

    Added functionality and comments to UIButtons
    All the UIButtons call their respective functions

--- Assets/Scripts/UI/Buttons/UOptionsButton.cs
+++ Assets/Scripts/UI/Buttons/UOptionsButton.cs
@@ -1,11 +1,24 @@
-﻿using System.Collections;
+﻿//FILE          :   UOptionsButton.cs
+//PROJECT       :   Will of the Wood
+//PROGRAMMER    :   Gavin McGuire
+//FIRST VERSION :   12/12/2019
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//NAME : UOptionsButton
+//PURPOSE : calls the function to open the options menu
 public class UOptionsButton : UIButton
 {
+
+    //Function : Clicked
+    //DESCRIPTION : Calls the function in the UI event manager that opens the options menu
+    // and closes the main menu
+    //PARAMETERS :  none
+    //RETURNS : none
     public override void Clicked()
     {
-        throw new System.NotImplementedException();
+        UIEventManager.TriggerCloseMainMenu();
+        UIEventManager.TriggerOptionsOpen();
     }
 }
