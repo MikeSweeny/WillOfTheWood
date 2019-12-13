5b46d0b352146e6ebcc6d4470a8bd1c53a534267
Gavin McGuire <44212243+Milowan@users.noreply.github.com>
2019-12-13 16:23:41 -0500

    created button scripts for the pause menu

---
+++ Assets/Scripts/UI/Buttons/UInventoryButton.cs
@@ -0,0 +1,23 @@
+﻿//FILE          :   UInventoryButton.cs
+//PROJECT       :   Will of the Wood
+//PROGRAMMER    :   Gavin McGuire
+//FIRST VERSION :   13/12/2019
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NAME : UInventoryButton
+//PURPOSE : opens the inventory menu
+public class UInventoryButton : UIButton
+{
+
+    //Function : Clicked
+    //DESCRIPTION : calls the function in the UI event manager that opens the inventory
+    //PARAMETERS : none
+    //RETURNS : none
+    public override void Clicked()
+    {
+        UIEventManager.TriggerClosePause();
+        UIEventManager.TriggerOpenInventory();
+    }
+}
---
+++ Assets/Scripts/UI/Buttons/ULevelingMenuButton.cs
@@ -0,0 +1,23 @@
+﻿//FILE          :   ULevelingMenuButton.cs
+//PROJECT       :   Will of the Wood
+//PROGRAMMER    :   Gavin McGuire
+//FIRST VERSION :   13/12/2019
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NAME : ULevelingMenuButton
+//PURPOSE : Opens the leveling menu
+public class ULevelingMenuButton : UIButton
+{
+
+    //Function : Clicked
+    //DESCRIPTION : calls the function in the UI event manager that opens the leveling menu
+    //PARAMETERS : none
+    //RETURNS : none
+    public override void Clicked()
+    {
+        UIEventManager.TriggerClosePause();
+        UIEventManager.TriggerOpenLeveling();
+    }
+}
