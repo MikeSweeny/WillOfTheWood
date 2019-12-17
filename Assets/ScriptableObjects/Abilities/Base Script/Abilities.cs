ef69878db8fd68175366ef9c9bc26d104caeb3a3
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2019-12-16 21:04:29 -0500

    Abillities/JHP Base Script and Assets
    -Fixed the parent script for abilities and created the first test child called 'Jump Higher Passive' ability script. Hoping this can be a good start to how we approach abilities.

---
+++ Assets/ScriptableObjects/Abilities/Base Script/Abilities.cs
@@ -0,0 +1,39 @@
+﻿//FILE : Ability.cs
+//PROJECT : Will of the Woods
+//PROGRAMMER : Jonathan Parsons
+//FIRST VERSION : 16/12/2019
+using UnityEngine;
+
+//NAME : Abilities Base Scriptable Object
+//PURPOSE :Parent used to create the abilities
+
+//Puts 'Ability' into the asset menu to be called in unity
+public class Abilities: ScriptableObject
+{
+    //Sets the name and description of ability
+    [SerializeField]
+    private string abilityName;
+    [SerializeField]
+    [TextArea(10, 10)]
+    private string abilityDescription;
+    [SerializeField]
+    private Sprite abilityImage;
+    //Variables that are applied when needed.
+    [SerializeField]
+    public bool isNovice = false;
+    [SerializeField]
+    public bool isAdept = false;
+    [SerializeField]
+    public bool isMaster = false;
+    protected bool isCollected = false;
+
+    public virtual void OnLoad() { }
+    public virtual void noviceLevelAct() { }
+    public virtual void adeptLevelAct() { }
+    public virtual void masterLevelAct() { }
+
+    private void OnDestroy()
+    {
+        isCollected = true;
+    }
+}
---
+++ Assets/ScriptableObjects/Abilities/Base Script/JumpHigher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New JHPassive", menuName = "Abilities/JumpHigherPassive")]
+public class JumpHigher : Abilities
+{
+    PlayerController player;
+    public override void OnLoad()
+    {
+        if(isCollected)
+        {
+            if (isNovice)
+            {
+                noviceLevelAct();
+            }
+            else if (isAdept)
+            {
+                adeptLevelAct();
+            }
+            else if (isMaster)
+            {
+                masterLevelAct();
+            }
+        }
+    }
+    public override void noviceLevelAct()
+    {
+        player.jumpSpeed = 77.0f;
+    }
+    public override void adeptLevelAct()
+    {
+        player.jumpSpeed = 79.0f;
+    }
+    public override void masterLevelAct()
+    {
+        player.jumpSpeed = 83.0f;
+    }
+}
