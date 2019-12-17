c28c16132b0d4d0b9b60535acd96b6206614ea42
Milowan <44212243+Milowan@users.noreply.github.com>
2019-12-17 16:34:12 -0500

    implemented HUDElement inheritance

--- Assets/Scripts/UI/HUD/HUDElement.cs
+++ Assets/Scripts/UI/HUD/HUDElement.cs
@@ -1,9 +1,19 @@
-﻿using System.Collections;
+﻿//FILE          :   HUDElement.cs
+//PROJECT       :   project
+//PROGRAMMER    :   Gavin McGuire
+//FIRST VERSION :   DD/MM/YYYY
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//NAME : HUDElement
+//PURPOSE : acts as an element in the HUD
 public abstract class HUDElement : MonoBehaviour
 {
 
+    //Function : UpdateElement
+    //DESCRIPTION : Updates the relevant information to the element
+    //PARAMETERS : none
+    //RETURNS : none
     public abstract void UpdateElement();
 }
--- Assets/Scripts/UI/HUD/PlayerHUD.cs
+++ Assets/Scripts/UI/HUD/PlayerHUD.cs
@@ -1,12 +1,21 @@
-﻿using System.Collections;
+﻿//FILE          :   PlayerHUD.cs
+//PROJECT       :   Will of the Wood
+//PROGRAMMER    :   Gavin McGuire
+//FIRST VERSION :   17/12/2019
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//NAME : PlayerHUD
+//PURPOSE : serves as the player HUD
 public class PlayerHUD : MonoBehaviour
 {
     private List<HUDElement> elements;
 
-    // Start is called before the first frame update
+    //Function : Awake
+    //DESCRIPTION : called when the object is initialized
+    //PARAMETERS : none
+    //RETURNS : none
     void Awake()
     {
         for (int i = 0; i < transform.childCount; ++i)
@@ -15,7 +24,11 @@ public class PlayerHUD : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
+
+    //Function : Update
+    //DESCRIPTION : called ever frame
+    //PARAMETERS : none
+    //RETURNS : none
     void Update()
     {
         foreach (HUDElement element in elements)
