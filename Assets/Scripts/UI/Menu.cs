8d101c64e726638655163157ee826bf5481fa1d0
Milowan <44212243+Milowan@users.noreply.github.com>
2019-12-17 14:36:44 -0500

    Folder cleanup pt 1
    reorganized menu folders to be more specific to each menu

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,31 @@
+﻿//FILE          :   Menu.cs
+//PROJECT       :   Will of the Wood
+//PROGRAMMER    :   Gavin McGuire
+//FIRST VERSION :   11/12/2019
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NAME : Menu
+//PURPOSE : Parent of all the menu classes
+public class Menu : MonoBehaviour
+{
+
+    //Function : OpenMenu
+    //DESCRIPTION : Activates the menu to "open" it
+    //PARAMETERS : none
+    //RETURNS : none
+    protected void OpenMenu()
+    {
+        gameObject.SetActive(true);
+    }
+
+    //Function : CloseMenu
+    //DESCRIPTION : Deactivates the menu to "close" it
+    //PARAMETERS : none
+    //RETURNS : none
+    protected void CloseMenu()
+    {
+        gameObject.SetActive(false);
+    }
+}
