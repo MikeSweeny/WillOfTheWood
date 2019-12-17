feef4d24053bf222924cf58371a51b145ee20ea0
Milowan <44212243+Milowan@users.noreply.github.com>
2019-12-17 14:37:16 -0500

    Folder cleanup pt 2

---
+++ Assets/Scripts/UI/Menus/LoadMenu/UCloseLoadButton.cs
@@ -0,0 +1,24 @@
+﻿//FILE          :   UCloseLoadButton.cs
+//PROJECT       :   Will of the Wood
+//PROGRAMMER    :   Gavin McGuire
+//FIRST VERSION :   17/12/2019
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NAME : UCloseLoadButton
+//PURPOSE : closes the load menu
+public class UCloseLoadButton : UIButton
+{
+
+    //Function : Clicked
+    //DESCRIPTION : calls the function in the UI event manager that opens the options menu
+    // and closes the load menu
+    //PARAMETERS : none
+    //RETURNS : none
+    public override void Clicked()
+    {
+        UIEventManager.TriggerCloseLoadMenu();
+        UIEventManager.TriggerOpenMainMenu();
+    }
+}
---
+++ Assets/Scripts/UI/Menus/LoadMenu/ULoadSlotSelectButton.cs
@@ -0,0 +1,23 @@
+﻿//FILE          :   ULoadSlotSelectButton
+//PROJECT       :   Will of the Wood
+//PROGRAMMER    :   Gavin McGuire
+//FIRST VERSION :   13/12/2019
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NAME : ULoadSlotSelectButton
+//PURPOSE : sets the desired slot for saving in the GameSaveManager and starts the game
+public class ULoadSlotSelectButton : UIButton
+{
+
+    //Function : Clicked
+    //DESCRIPTION : sets the current slot string to the name of the parent containing this button
+    //PARAMETERS : none
+    //RETURNS : none
+    public override void Clicked()
+    {
+        GameSaveManager.SetCurrentSlot(transform.parent.name);
+        //load desired scene here
+    }
+}
