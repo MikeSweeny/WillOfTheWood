29b6e1d97acf48bbc466f3c8c975034f15821a43
Milowan <44212243+Milowan@users.noreply.github.com>
2019-12-19 16:18:41 -0500

    created functionality to populate quest menu

--- Assets/Scripts/UI/Menus/Quest/MQuest.cs
+++ Assets/Scripts/UI/Menus/Quest/MQuest.cs
@@ -5,11 +5,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 //NAME : MQuest
 //PURPOSE : serves as the quest menu
 public class MQuest : Menu
 {
+
+    private Transform detailsBox;
+    private Text title;
+    private Text description;
+    private Text requirements;
+    private Text reward;
+    private Quests selectedQuest;
 
     //Function : Awake
     //DESCRIPTION : called when the object is initialized
@@ -18,8 +26,20 @@ public class MQuest : Menu
     void Awake()
     {
         gameObject.SetActive(false);
+        detailsBox = transform.Find("DescriptionHolder");
+        title = detailsBox.Find("QuestTitleText").GetComponent<Text>();
+        description = detailsBox.Find("DescriptionText").GetComponent<Text>();
+        requirements = detailsBox.Find("RequirementsText").GetComponent<Text>();
+        reward = detailsBox.Find("RewardsText").GetComponent<Text>();
         UIEventManager.OpenQuests += OpenMenu;
         UIEventManager.CloseQuests += CloseMenu;
+    }
+
+    private void Update()
+    {
+        title.text = selectedQuest.QuestName;
+        description.text = selectedQuest.Description;
+        requirements.text = selectedQuest.QuestObjectives;
     }
 
     //Function : OnDestroy
@@ -32,4 +52,9 @@ public class MQuest : Menu
         UIEventManager.CloseQuests -= CloseMenu;
     }
 
+
+    public void SetQuest(Quests quest)
+    {
+        selectedQuest = quest;
+    }
 }
