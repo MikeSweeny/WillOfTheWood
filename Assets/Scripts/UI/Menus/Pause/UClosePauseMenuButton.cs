074a5307d0ec3c1a3ff285238a27c2123c5d2180
Milowan <44212243+Milowan@users.noreply.github.com>
2019-12-22 09:52:34 -0500

    fixed pause / unpause

---
+++ Assets/Scripts/UI/Menus/Pause/UClosePauseMenuButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UClosePauseMenuButton : UIButton
+{
+    public override void Clicked()
+    {
+        GameEventManager.TriggerUnPause();
+    }
+}
