fe2394bad70544431cbed80379d7e5716175933a
Rebecca Stewart <44781452+beckrpgirl@users.noreply.github.com>
2020-01-06 15:25:49 -0500

    update on Collectable quest
    update on collectable quests

--- Assets/Scripts/Quests/QuestsSO/QuestScripts/CollectableQuest.cs
+++ Assets/Scripts/Quests/QuestsSO/QuestScripts/CollectableQuest.cs
@@ -2,7 +2,7 @@
 //PROJECT : Will of the Woods
 //PROGRAMMER : Rebecca Stewart
 //FIRST VERSION : 06/12/2019
-
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Quest", menuName = "Quests/Collectable")]
@@ -10,12 +10,23 @@ using UnityEngine;
 //PURPOSE : Holds the requirments for the collectable quest
 public class CollectableQuest : Quests
 {
+    public List<string> CNPC = new List<string>();
     public override void Load()
     {
         CurrentAmount = 0;
         Completed = false;
         Goals.Add(new CollectingQuest(this, NPCID, "Kill " + RequiredAmount + " " + NPCID, false, CurrentAmount, RequiredAmount, CoinReward));
         Goals.ForEach(g => g.Init());
+
+        for (int i = 0; i < CNPC.Count; i++)
+        {
+            GameObject NPC;
+            NPC = GameObject.Find(CNPC[i]);
+            if (NPC)
+            {
+                NPC.GetComponent<ConversationNpc>().hasBeenTalkedTo = false;
+            }
+        }
     }
 
 
