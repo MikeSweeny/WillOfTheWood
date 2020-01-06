9048d015f2a6e4c438094465a372ca0ed048c100
Grizzly2487 <52928837+Grizzly2487@users.noreply.github.com>
2020-01-06 13:19:12 -0500

    Made Weapons
    Made all the weapon scripts for the different types of weapon objects. Can now be attached to the different weapon objects prefabs in game.

---
+++ Assets/Scripts/Items/Base Scripts/Weapons/GreatSword.cs
@@ -0,0 +1,43 @@
+﻿//FILE          :   GreatSword.cs
+//PROJECT       :   Will of the Wood
+//PROGRAMMER    :   Jonathan Parsons
+//FIRST VERSION :   7/1/2020
+using UnityEngine;
+
+//NAME : Mace
+//PURPOSE : The child class of iWeapon for the GreatSword weapon
+public class GreatSword : IWeapon
+{
+    CharacterBase character;
+    private int roll;
+
+    //Function : start
+    //DESCRIPTION : sets the stats for GreatSword on start
+    //PARAMETERS : none
+    //RETURNS : none
+    private void Start()
+    {
+        stats = new GreatSwordStats();
+    }
+    //Function : OnTriggerEnter
+    //DESCRIPTION : checks the collider for the object being hit and applies damage to that object
+    //PARAMETERS : none
+    //RETURNS :none
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.root.tag != gameObject.transform.root.tag)
+        {
+            character = other.gameObject.GetComponent<CharacterBase>();
+            character.TakeDamage(AttackRoll());
+        }
+    }
+    //Function : OnTriggerEnter
+    //DESCRIPTION : checks the weapons min and max damage and randomizes a number between them, takes that number and applies it as the roll.
+    //PARAMETERS : attackMinDamage, attackMaxDamage
+    //RETURNS : int roll
+    public int AttackRoll()
+    {
+        roll = (int)Random.Range(stats.GetAttackMinDamage(), stats.GetAttackMaxDamage());
+        return roll;
+    }
+}
---
+++ Assets/Scripts/Items/Base Scripts/Weapons/ShortSword.cs
@@ -0,0 +1,43 @@
+﻿//FILE          :   ShortSword.cs
+//PROJECT       :   Will of the Wood
+//PROGRAMMER    :   Jonathan Parsons
+//FIRST VERSION :   7/1/2020
+using UnityEngine;
+
+//NAME : ShortSword
+//PURPOSE : The child class of iWeapon for the shortsword weapon
+public class ShortSword : IWeapon
+{
+    CharacterBase character;
+    private int roll;
+
+    //Function : start
+    //DESCRIPTION : sets the stats for sword on start
+    //PARAMETERS : none
+    //RETURNS : none
+    private void Start()
+    {
+        stats = new ShortSwordStats();
+    }
+    //Function : OnTriggerEnter
+    //DESCRIPTION : checks the collider for the object being hit and applies damage to that object
+    //PARAMETERS : none
+    //RETURNS :none
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.root.tag != gameObject.transform.root.tag)
+        {
+            character = other.gameObject.GetComponent<CharacterBase>();
+            character.TakeDamage(AttackRoll());
+        }
+    }
+    //Function : OnTriggerEnter
+    //DESCRIPTION : checks the weapons min and max damage and randomizes a number between them, takes that number and applies it as the roll.
+    //PARAMETERS : attackMinDamage, attackMaxDamage
+    //RETURNS : int roll
+    public int AttackRoll()
+    {
+        roll = (int)Random.Range(stats.GetAttackMinDamage(), stats.GetAttackMaxDamage());
+        return roll;
+    }
+}
