ad79dc6fc2400021c5dc05e44dc3d1319c380612
Milowan <44212243+Milowan@users.noreply.github.com>
2020-01-06 16:10:57 -0500

    implemented inventory slot images in scripts

--- Assets/Scripts/Items/Base Scripts/IPlayerInventory.cs
+++ Assets/Scripts/Items/Base Scripts/IPlayerInventory.cs
@@ -11,4 +11,38 @@ using UnityEngine.UI;
 //PURPOSE : handles all player inventory specific logic
 public class IPlayerInventory : Inventory
 {
+    private MInventory inventoryMenu;
+
+    //Function : Start
+    //DESCRIPTION : called when the object is initialized after Awake
+    //PARAMETERS : none
+    //RETURNS : none
+    private void Start()
+    {
+        inventoryMenu = GameObject.Find("InventoryCanvas").GetComponent<MInventory>();
+        
+    }
+
+    //Function : AddItem
+    //DESCRIPTION : adds an item to the inventory so long as the inventory is not full
+    //PARAMETERS : Item item : the item to add to the inventory
+    //RETURNS : none
+    public override void AddItem(Item item)
+    {
+        if (inventoryMenu.GetCurrentCount() + 1 < inventoryMenu.GetMaxCount())
+        { 
+            base.AddItem(item);
+            inventoryMenu.IncrementCurrentCount();
+        }
+    }
+
+    //Function : RemoveItem
+    //DESCRIPTION : removes an item from the inventory
+    //PARAMETERS : Item item : the item to remove from the inventory
+    //RETURNS : none
+    public override void RemoveItem(Item item)
+    {
+        base.RemoveItem(item);
+        inventoryMenu.DecrementCurrentCount();
+    }
 }
--- Assets/Scripts/UI/Menus/Inventory/MInventory.cs
+++ Assets/Scripts/UI/Menus/Inventory/MInventory.cs
@@ -13,6 +13,9 @@ public class MInventory : Menu
 {
 
     protected List<Image> slotImages;
+    private Image defaultSlotImage;
+    private int invContentsCount;
+    private int maxContentsCount;
 
     //Function : Awake
     //DESCRIPTION : called when the object is initialized
@@ -24,6 +27,13 @@ public class MInventory : Menu
         slotImages = new List<Image>();
         UIEventManager.OpenInventory += OpenMenu;
         UIEventManager.CloseInventory += CloseMenu;
+        Transform slotHolder = transform.Find("InventorySlotHolder");
+        maxContentsCount = slotHolder.childCount;
+        for (int i = 0; i < maxContentsCount; ++i)
+        {
+            slotImages.Add(transform.GetChild(i).GetComponent<Image>());
+        }
+        defaultSlotImage.Equals(slotImages[0]);
     }
 
     //Function : OnDestroy
@@ -34,5 +44,60 @@ public class MInventory : Menu
     {
         UIEventManager.OpenInventory -= OpenMenu;
         UIEventManager.CloseInventory -= CloseMenu;
+    }
+
+    //Function : GetMaxCount
+    //DESCRIPTION : gets the max number of items in the inventory
+    //PARAMETERS : none
+    //RETURNS : int maxContentsCount : the maximum amount of items in the inventory
+    public int GetMaxCount()
+    {
+        return maxContentsCount;
+    }
+
+    //Function : GetCurrentCount
+    //DESCRIPTION : gets the current number of items in the inventory
+    //PARAMETERS : none
+    //RETURNS : int invContentsCount : the current amount of items in the inventory
+    public int GetCurrentCount()
+    {
+        return invContentsCount;
+    }
+
+    //Function : IncrementCurrentCount
+    //DESCRIPTION : increments the current content count
+    //PARAMETERS : none
+    //RETURNS : none
+    public void IncrementCurrentCount()
+    {
+        invContentsCount++;
+    }
+
+    //Function : IncrementCurrentCount
+    //DESCRIPTION : decrements the current content count
+    //PARAMETERS : none
+    //RETURNS : none
+    public void DecrementCurrentCount()
+    {
+        invContentsCount--;
+    }
+
+    //Function : UpdateSlots
+    //DESCRIPTION : updates inventory menu slots
+    //PARAMETERS : List<Item> inventory : the player inventory
+    //RETURNS : none
+    public void UpdateSlots(List<Item> inventory)
+    {
+        for (int i = 0; i < maxContentsCount; ++i)
+        {
+            if (i < invContentsCount)
+            {
+                slotImages[i] = inventory[i].GetSprite();
+            }
+            else
+            {
+                slotImages[i] = defaultSlotImage;
+            }
+        }
     }
 }
