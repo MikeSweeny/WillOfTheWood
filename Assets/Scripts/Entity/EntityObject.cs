6e795556a07abf46fa19610acd8edca11364d0d7
eitrunix <52928831+eitrunix@users.noreply.github.com>
2020-01-08 09:46:47 -0500

    ....

--- Assets/Scripts/Entity/EntityObject.cs
+++ Assets/Scripts/Entity/EntityObject.cs
@@ -1,58 +1,58 @@
-﻿using UnityEngine;
-//File Header: EntityObject
-//FILE : EntityObject.cs
-//PROJECT : WillOfTheWood
-//PROGRAMMER : Jeff Oldfield    
-//FIRST VERSION : 06/12/2019
+﻿//using UnityEngine;
+////File Header: EntityObject
+////FILE : EntityObject.cs
+////PROJECT : WillOfTheWood
+////PROGRAMMER : Jeff Oldfield    
+////FIRST VERSION : 06/12/2019
 
-//Class Header:
-//NAME : EnttiyObject : ScriptableObject
-//PURPOSE : To setup and initialize base Game Entities like the Player, Enemies and NPCs
+////Class Header:
+////NAME : EnttiyObject : ScriptableObject
+////PURPOSE : To setup and initialize base Game Entities like the Player, Enemies and NPCs
 
-public enum EntityType
-{
-    Player,
-    Enemy,
-    NPC,
-    Other
-}
-//Class Header:
-//NAME : EnttiyObject : ScriptableObject
-//PURPOSE : Building Entities to be used i nthe game world with default perameters set.
+//public enum EntityType
+//{
+//    Player,
+//    Enemy,
+//    NPC,
+//    Other
+//}
+////Class Header:
+////NAME : EnttiyObject : ScriptableObject
+////PURPOSE : Building Entities to be used i nthe game world with default perameters set.
 
-public abstract class EntityObject : ScriptableObject
-{
-    public  new string name;
-    public int ID;
-    [TextArea(10, 10)]
-    public string description;
-    public EntityType type;
+//public abstract class EntityObject : ScriptableObject
+//{
+//    public  new string name;
+//    public int ID;
+//    [TextArea(10, 10)]
+//    public string description;
+//    public EntityType type;
 
-    public virtual void CalcToughness() { }
+//    public virtual void CalcToughness() { }
 
-}
+//}
 
-//Class Header:
-//NAME : Entiy
-//PURPOSE : To setup and initialize base Game Entities like the Player, Enemies and NPCs
+////Class Header:
+////NAME : Entiy
+////PURPOSE : To setup and initialize base Game Entities like the Player, Enemies and NPCs
 
-public class Entity
-{
-    public string name;
-    public int ID;
-    public EntityType type;
+//public class Entity
+//{
+//    public string name;
+//    public int ID;
+//    public EntityType type;
 
-    //Function Header:
-    //FUNCTION : Entitiy()
-    //DESCRIPTION : Initialize child Classes with the information set in the Entity : ScriptableObjects class
-    //PARAMETERS : EntityObject entity
-    //RETURNS : VOID
+//    //Function Header:
+//    //FUNCTION : Entitiy()
+//    //DESCRIPTION : Initialize child Classes with the information set in the Entity : ScriptableObjects class
+//    //PARAMETERS : EntityObject entity
+//    //RETURNS : VOID
 
-    public Entity(EntityObject entity)
-    {
-        name = entity.name;
-        ID = entity.ID;
-        type = entity.type;
-    }
+//    public Entity(EntityObject entity)
+//    {
+//        name = entity.name;
+//        ID = entity.ID;
+//        type = entity.type;
+//    }
    
-}
+//}
--- Assets/Scripts/Entity/NPCObject.cs
+++ Assets/Scripts/Entity/NPCObject.cs
@@ -1,20 +1,20 @@
 ﻿
-////FILE : NPCObject.cs
-////PROJECT : WillofTheWood
-////PROGRAMMER : Jeff Oldfield
-////FIRST VERSION : 08/12/2019
+//////FILE : NPCObject.cs
+//////PROJECT : WillofTheWood
+//////PROGRAMMER : Jeff Oldfield
+//////FIRST VERSION : 08/12/2019
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-////NAME : NPCObject : EntityObject
-////PURPOSE : Screating NPCs
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
 
+//////NAME : NPCObject : EntityObject
+//////PURPOSE : Screating NPCs
 
-public class NPCObject : EntityObject
-{
 
-}
+
+//public class NPCObject : EntityObject
+//{
+
+//}
