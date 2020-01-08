29aed0c479b1f3e23bdd668fc8db00aa2bc4cfd7
Milowan <44212243+Milowan@users.noreply.github.com>
2020-01-08 14:59:55 -0500

    Inventory slot sprites now change

---
+++ Assets/Scripts/UI/Menus/Inventory/InventorySlot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    private Image image;
+
+    private void Awake()
+    {
+        image = transform.GetChild(0).GetChild(0).GetComponent<Image>();
+    }
+
+    public void SetImageSprite(Sprite sprite)
+    {
+        image.sprite = sprite;
+    }
+
+    public Sprite GetImageSprite()
+    {
+        return image.sprite;
+    }
+}
