7e071e2fb27de62cb30aea3731a9e0ccf99c5a2c
TimothyOlenio <52928854+TimothyOlenio@users.noreply.github.com>
2020-01-08 14:19:56 -0500

    Created Follow Script
    This script makes the attached object follow the tag that you pass in into the editor, currently conflicts with the NPC controller

--- Assets/Scripts/NPCs/FollowPlayer.cs
+++ Assets/Scripts/NPCs/FollowPlayer.cs
@@ -1,18 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FollowPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private NavMeshAgent agent;
+
+    private bool StopFollowing;
+    private bool BeginFollowing;
+    public string TargetTag;
+
+    private GameObject Target;
+    private Transform ToTarget;
+
+    private void Awake()
     {
-        
+        StopFollowing = false;
+        BeginFollowing = false;
+        Target = GameObject.FindGameObjectWithTag(TargetTag);
+        ToTarget = Target.transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void FixedUpdate()
+    {            
+        BeginFollowing = true;
+
+        if (BeginFollowing)
+        {
+            agent.SetDestination(ToTarget.position);
+        }
     }
 }
