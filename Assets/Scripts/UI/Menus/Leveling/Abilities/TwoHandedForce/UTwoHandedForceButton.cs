15e3a015edaf2f1ee9fafffcfb67a658eee06412
Milowan <44212243+Milowan@users.noreply.github.com>
2020-01-09 13:29:15 -0500

    generated scripts for ability menu pt3

--- Assets/Scripts/UI/Menus/Leveling/Abilities/TwoHandedForce/UTwoHandedForceButton.cs
+++ Assets/Scripts/UI/Menus/Leveling/Abilities/TwoHandedForce/UTwoHandedForceButton.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UAbilitySlotButton : MonoBehaviour
+public class UTwoHandedForceButton : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
