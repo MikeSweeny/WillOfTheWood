3fbe75c42caf2904dd9299f99fffdf634666b553
Milowan <44212243+Milowan@users.noreply.github.com>
2020-01-09 15:31:38 -0500

    renamed class and file

---
+++ Assets/Scripts/UI/Menus/Leveling/Abilities/ExeptionalAttribute/UExceptionalAttributeButton.cs
@@ -0,0 +1,25 @@
+﻿//FILE          :   UExceptionalAttributeButton.cs
+//PROJECT       :   Will of the Wood
+//PROGRAMMER    :   Gavin McGuire
+//FIRST VERSION :   09/01/2020
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//NAME : UExceptionalAttributeButton
+//PURPOSE : sets the selected ability in the 
+//leveling menu to the ExceptionalAttribute ability
+public class UExceptionalAttributeButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
