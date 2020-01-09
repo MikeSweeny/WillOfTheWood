6f5c0f2317986028d52a606110ffb8d4e6552106
Milowan <44212243+Milowan@users.noreply.github.com>
2020-01-09 13:25:02 -0500

    generated scripts for ability menu pt1

---
+++ Assets/Scripts/UI/Menus/Leveling/Abilities/AtArms/UUnlockAtArmsButton.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UUnlockAtArmsButton : UIButton
+{
+    public override void Clicked()
+    {
+        AbilityManager.TriggerUnlockAtArms();
+    }
+}
