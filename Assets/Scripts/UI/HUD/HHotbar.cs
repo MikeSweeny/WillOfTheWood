3d08ee63e3f0379373635b0a336d75d1a998b299
Milowan <44212243+Milowan@users.noreply.github.com>
2020-01-10 13:25:18 -0500

    prepwork for hotbar HUD info and fixed quest menu exit button

--- Assets/Scripts/UI/HUD/HHotbar.cs
+++ Assets/Scripts/UI/HUD/HHotbar.cs
@@ -10,6 +10,25 @@ using UnityEngine;
 //PURPOSE : displays the abilities / items that are bound to the hotbar
 public class HHotbar : HUDElement
 {
+    private Transform hotbarHolder1;
+    private Transform hotbarHolder2;
+
+    private void Start()
+    {
+        hotbarHolder1 = transform.Find("LeftHotbarHolder");
+        hotbarHolder2 = transform.Find("RightHotbarHolder");
+        for (int i = 0; i < player.GetHotbar().Count; ++i)
+        {
+            if (i < hotbarHolder1.childCount)
+            {
+                hotbarHolder1.GetChild(i).GetComponent<HotbarHUDSlot>().SetHotbarSlot(player.GetHotbar()[i]);
+            }
+            if (i >= hotbarHolder1.childCount)
+            {
+                hotbarHolder2.GetChild(i - hotbarHolder1.childCount).GetComponent<HotbarHUDSlot>().SetHotbarSlot(player.GetHotbar()[i]);
+            }
+        }
+    }
 
     //Function : UpdateElement
     //DESCRIPTION : Updates the relevant information to the element
