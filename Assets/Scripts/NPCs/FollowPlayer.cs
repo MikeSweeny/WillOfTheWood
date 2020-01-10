1d4dd49e3953fce4bfb87c338016dd5cba05bbef
Rebecca Stewart <44781452+beckrpgirl@users.noreply.github.com>
2020-01-09 20:03:27 -0500

    escort movement
    working on walking for the escort character

--- Assets/Scripts/NPCs/FollowPlayer.cs
+++ Assets/Scripts/NPCs/FollowPlayer.cs
@@ -14,30 +14,43 @@ public class FollowPlayer : BaseNpc
 
     private bool StopFollowing;
     public bool BeginFollowing;
-
+    protected bool CanWalk;
+    protected Animator characterAnim;
+   
     public string TargetTag;
     public string CompletionArea;
 
     private GameObject Target;
     private Transform ToTarget;
 
+
+
     private void Awake()
     {
         StopFollowing = false;
         BeginFollowing = false;
-        
+        characterAnim = GetComponent<Animator>();
+
 
         pathingPattern = GetComponent<NpcPathing>();
         SetPathingComp();
+        
     }
 
-    private void FixedUpdate()
-    {            
+    private void Update()
+    {
 
+    }
+    private void FixedUpdate()
+    {
+        
         if (BeginFollowing)
         {
             GetTargetPosition();
             pathingPattern.FollowPlayer(Target);
+            SpeedCheck();
+            //characterAnim.Play("Walking");
+
         }
         else if (StopFollowing)
         {
@@ -46,6 +59,23 @@ public class FollowPlayer : BaseNpc
         }
     }
 
+    private void SpeedCheck()
+    {
+
+        if (Target.transform.position == gameObject.transform.position)
+        {
+            CanWalk = false;
+            characterAnim.SetBool("CanWalk", false);
+
+        }
+        else
+        {
+            CanWalk = true;
+            characterAnim.SetBool("CanWalk", true);
+        }
+
+
+    }
     private void GetTargetPosition()
     {
         Target = GameObject.FindGameObjectWithTag(TargetTag);
